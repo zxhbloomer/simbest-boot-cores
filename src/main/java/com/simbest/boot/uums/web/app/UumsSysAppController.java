1f96180c062f2835e56a0b93a26c4bc1ae8bb761
lm8580552 <33541086+lm8580552@users.noreply.github.com>
2018-08-09 09:29:54 +0800

    新增api接口

---
+++ src/main/java/com/simbest/boot/uums/web/app/UumsSysAppController.java
@@ -0,0 +1,53 @@
+/*
+ * 版权所有 © 北京晟壁科技有限公司 2008-2027。保留一切权利!
+ */
+package com.simbest.boot.uums.web.app;
+
+import com.simbest.boot.base.web.response.JsonResponse;
+import com.simbest.boot.uums.api.app.UumsSysAppApi;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <strong>Title : SysAppController</strong><br>
+ * <strong>Description : </strong><br>
+ * <strong>Create on : 2018/5/26/026</strong><br>
+ * <strong>Modify on : 2018/5/26/026</strong><br>
+ * <strong>Copyright (C) Ltd.</strong><br>
+ *
+ * @author LM liumeng@simbest.com.cn
+ * @version <strong>V1.0.0</strong><br>
+ *          <strong>修改历史:</strong><br>
+ *          修改人 修改日期 修改描述<br>
+ *          -------------------------------------------<br>
+ */
+
+@Api (description = "应用决策群组相关接口" )
+@RestController
+@RequestMapping("/uums/sys/app")
+public class UumsSysAppController {
+
+    @Autowired
+    private UumsSysAppApi uumsSysAppApi;
+
+    /**
+     * 根据appCode查询应用的消息
+     * @param appcode
+     * @return
+     */
+    @ApiOperation(value = "根据appCode查询应用的消息", notes = "根据appCode查询应用的消息")
+    @ApiImplicitParam(name = "appcode", value = "应用code", dataType = "String", paramType = "query")
+    @PostMapping("/findAppByAppCode")
+    public JsonResponse findById(@RequestParam(required = false)  String appcode) {
+        return JsonResponse.success(uumsSysAppApi.findAppByAppCode( appcode ));
+    }
+
+}
+
+
