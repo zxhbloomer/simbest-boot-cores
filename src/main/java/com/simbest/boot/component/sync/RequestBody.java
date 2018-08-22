433c39a09af838bb818f52b1a48c322593e39c11
Kyne <lishuyi@simbest.com.cn>
2018-08-22 11:48:11 +0800

    增加对外提供接口的请求体

---
+++ src/main/java/com/simbest/boot/component/sync/RequestBody.java
@@ -0,0 +1,74 @@
+/*
+ * 版权所有 © 北京晟壁科技有限公司 2008-2027。保留一切权利!
+ */
+package com.simbest.boot.component.sync;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.Date;
+
+/**
+ * 用途：用于对外提供接口的请求体，适用于对外提供/anonymous/**路径的接口鉴权
+ * 作者: lishuyi
+ * 时间: 2018/8/22  11:45
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestBody<T> {
+
+    //请求方IP
+    @NonNull
+    private String clientIp;
+
+    //请求方应用代码
+    @NonNull
+    private String clientCode;
+
+    //请求方应用名称
+    @NonNull
+    private String clientName;
+
+    //请求方联系人
+    @NonNull
+    private String clientLinkman;
+
+    //请求方联系人电话
+    @NonNull
+    private String clientLinkTel;
+
+    //请求方联系人邮箱
+    @NonNull
+    private String clientLinkEmail;
+
+    //请求方局方联系人
+    @NonNull
+    private String customLinkman;
+
+    //请求方局方联系人电话
+    @NonNull
+    private String customLinkTel;
+
+    //请求方局方联系人邮箱
+    @NonNull
+    private String customLinkEmail;
+
+    //请求时间
+    @NonNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date requestTime;
+
+    //请求数据
+    @NonNull
+    private T data;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
