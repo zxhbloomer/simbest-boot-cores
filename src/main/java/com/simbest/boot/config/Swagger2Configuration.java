4895afc469748a4b7790d75d6561f65265e8414c
Kyne <lishuyi@simbest.com.cn>
2018-10-16 11:27:41 +0800

    修改Swagger在WebMvcConfigurationSupport下的配置

--- src/main/java/com/simbest/boot/config/Swagger2Configuration.java
+++ src/main/java/com/simbest/boot/config/Swagger2Configuration.java
@@ -6,16 +6,14 @@ package com.simbest.boot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.service.Contact;
 
 /**
  * 用途：RESTFUL API 在线文档
@@ -26,15 +24,7 @@ import springfox.documentation.service.Contact;
 @Configuration
 @EnableSwagger2
 @Profile(value = {"dev", "test", "uat"})
-public class Swagger2Configuration extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars*")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+public class Swagger2Configuration {
 
     @Bean
     public Docket buildDocket() {
--- src/main/java/com/simbest/boot/config/WebMvcConfigSupport.java
+++ src/main/java/com/simbest/boot/config/WebMvcConfigSupport.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.resource.ContentVersionStrategy;
@@ -48,6 +47,11 @@ public class WebMvcConfigSupport extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
                 .addVersionStrategy(new ContentVersionStrategy(), "/**");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars*")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
 
         registry.addResourceHandler(
                 "/webjars/**",
